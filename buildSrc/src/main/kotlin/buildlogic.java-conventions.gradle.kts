dbb480d096b71c0a41c884652d5a7521be02ad3e
mohibazim26-cmd <mohibazim26@gmail.com>
2026-06-15 19:19:04 +0200

    Update buildlogic.java-conventions.gradle.kts

--- buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/buildlogic.java-conventions.gradle.kts
@@ -16,11 +16,13 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
+
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile>() {
     options.encoding = "UTF-8"
-    options.release.set(21)
 }
 
 tasks.withType<Javadoc>() {
